27419c95cd8ef82fb60b4317787b009fac2dd5bb
DaniSanxes <adam23dsanchez@inslaferreria.cat>
2024-04-16 20:32:36 +0200

    Prinicipal

---
+++ ExamenM5/src/main/java/danisanxess/examenm5/ExamenM5.java
@@ -0,0 +1,17 @@
+
+package danisanxess.examenm5;
+
+/**
+ *
+ * @author Daniel Sánchez
+ */
+public class ExamenM5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
